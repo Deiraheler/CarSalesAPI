92c130cd62ad9bc0c24c206e8d4329328e74856f
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2024-04-17 12:53:56 +0100

    Init

--- index.mjs
+++ index.mjs
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
-import https from "https";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
 import posts from "./routes/posts.mjs";
@@ -12,24 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Load the /cars routes
+// Load the /posts routes
 app.use("/cars", posts);
 
 // Global error handling
 app.use((err, _req, res, next) => {
-  res.status(500).send("Uh oh! An unexpected error occurred.")
-});
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
 
-// Define HTTPS server options
-const httpsOptions = {
-  key: fs.readFileSync('/home/ec2-user/key.pem'), // Path to the key.pem file
-  cert: fs.readFileSync('/etc/ssl/cert.pem') // Path to the cert.pem file
-};
-
-// Create an HTTPS server with your configuration
-const server = https.createServer(httpsOptions, app);
-
-// Start the HTTPS server
-server.listen(PORT, () => {
-  console.log(`Server is running securely on port: ${PORT}`);
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
